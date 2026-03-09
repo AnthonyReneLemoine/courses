a23e33a948397536e0347ac711f4b8f6f19f709f
AnthonyReneLemoine <anthony.lemoine@gmail.com>
2026-03-09 19:28:09 +0100

    Fix Firebase config with valid production credentials

---
+++ firebase-config.example.js
@@ -0,0 +1,9 @@
+// Copiez ce fichier en `firebase-config.js` puis remplacez les valeurs.
+window.FIREBASE_CONFIG = {
+  apiKey: 'VOTRE_API_KEY',
+  authDomain: 'votre-projet.firebaseapp.com',
+  projectId: 'votre-projet',
+  storageBucket: 'votre-projet.appspot.com',
+  messagingSenderId: '000000000000',
+  appId: '1:000000000000:web:xxxxxxxxxxxx'
+};
---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// Configuration Firebase du projet.
+window.FIREBASE_CONFIG = {
+  apiKey: 'AIzaSyBQ6oM7sJsuczRlNBeE7i86FygQ_kBTlxU',
+  authDomain: 'courses-28e86.firebaseapp.com',
+  projectId: 'courses-28e86',
+  storageBucket: 'courses-28e86.firebasestorage.app',
+  messagingSenderId: '201148101093',
+  appId: '1:201148101093:web:e2d56fea6eea0d69fbbf0c'
+};
